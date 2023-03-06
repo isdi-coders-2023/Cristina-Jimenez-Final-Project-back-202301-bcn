02d127de88d1445804fe7ccde35ee42bf09dc4d2
Cristina <cristinajmartin98@gmail.com>
2023-03-06 22:29:20 +0100

    Add pong endpoint

--- src/server/controllers/pongController/pongController.ts
+++ src/server/controllers/pongController/pongController.ts
@@ -1,7 +1,7 @@
 import { type Request, type Response } from "express";
 
-const pong = (req: Request, res: Response) => {
+const pongController = (req: Request, res: Response) => {
   res.status(200).json({ pong: "pong!" });
 };
 
-export default pong;
+export default pongController;
