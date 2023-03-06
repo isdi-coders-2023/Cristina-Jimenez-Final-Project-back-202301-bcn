d5e66a21f8b3fbd19ec5b87afb0225687a273537
Cristina <cristinajmartin98@gmail.com>
2023-03-06 20:51:12 +0100

    Connect to the database, with debug logs feedback and error handling

--- src/index.ts
+++ src/index.ts
@@ -2,12 +2,17 @@ import "./loadEnvironment.js";
 import chalk from "chalk";
 import createDebug from "debug";
 import startServer from "./server/startServer.js";
+import connectDatabase from "./database/connectDatabase.js";
 
 const port = process.env.PORT ?? 4321;
+const mongoDbUrl = process.env.MONGODB_CONNECTION_URL!;
 
 export const debug = createDebug("pokedex:");
 
 try {
+  await connectDatabase(mongoDbUrl);
+  debug(chalk.cyanBright("Connected to database"));
+
   await startServer(+port);
   debug(chalk.green(`Server listening on port ${chalk.yellowBright(port)}`));
 } catch (error: unknown) {
