39c772acfdff24a68c700f3617edcb22367ba8cf
Cristina <cristinajmartin98@gmail.com>
2023-03-06 21:13:58 +0100

    Add generalError middleware

---
+++ src/server/middlewares/errorMiddlewares/errorMiddlewares.ts
@@ -0,0 +1,18 @@
+import { type NextFunction, type Request, type Response } from "express";
+import createDebug from "debug";
+import { type CustomError } from "../../../CustomError/CustomError.js";
+
+export const debug = createDebug("pokedex:server:middlewares:errorMiddleware");
+
+export const generalError = (
+  error: CustomError,
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  debug(error.message);
+
+  res
+    .status(error.statusCode || 500)
+    .json({ error: error.publicMessage || "Something went wrong" });
+};
