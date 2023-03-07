dbd2e6be224427ad7eb3977eff7ec814ce286d71
Cristina <cristinajmartin98@gmail.com>
2023-03-07 12:16:55 +0100

    Add loginUser controller

---
+++ src/server/controllers/userControllers/userControllers.ts
@@ -0,0 +1,60 @@
+import { type NextFunction, type Request, type Response } from "express";
+import bcrypt from "bcryptjs";
+import jwt from "jsonwebtoken";
+import { CustomError } from "../../../CustomError/CustomError";
+import User from "../../../database/models/User";
+import { type UserCredentials } from "../../types";
+import { type CustomJwtPayload } from "./types";
+
+export const loginUser = async (
+  req: Request<
+    Record<string, unknown>,
+    Record<string, unknown>,
+    UserCredentials
+  >,
+  res: Response,
+  next: NextFunction
+) => {
+  try {
+    const { password, username } = req.body;
+
+    const user = await User.findOne({ username }).exec();
+
+    if (!user) {
+      const userError = new CustomError(
+        "Wrong username",
+        401,
+        "Wrong credentials"
+      );
+
+      next(userError);
+
+      return;
+    }
+
+    if (!(await bcrypt.compare(password, user.password))) {
+      const userError = new CustomError(
+        "Wrong password",
+        401,
+        "Wrong credentials"
+      );
+
+      next(userError);
+
+      return;
+    }
+
+    const jwtPayload: CustomJwtPayload = {
+      sub: user?._id.toString(),
+      username,
+    };
+
+    const token = jwt.sign(jwtPayload, process.env.JWT_SECRET!, {
+      expiresIn: "2d",
+    });
+
+    res.status(200).json({ token });
+  } catch (error) {
+    next((error as Error).message);
+  }
+};
