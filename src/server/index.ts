ebd4e4b1a01328c34bc3f0a056cc863ed25908cc
Cristina <cristinajmartin98@gmail.com>
2023-03-07 11:53:01 +0100

    Add cors middleware with origins config

--- src/server/index.ts
+++ src/server/index.ts
@@ -1,11 +1,20 @@
 import express from "express";
 import morgan from "morgan";
+import cors from "cors";
 import pongController from "./controllers/pongController/pongController.js";
 
 export const app = express();
 
 app.disable("x-powered-by");
 
+const allowedOrigins = [
+  process.env.CORS_ALLOWED_LOCAL!,
+  process.env.CORS_ALLOWED_PRODUCTION!,
+];
+
+const corsOptions: cors.CorsOptions = { origin: allowedOrigins };
+
+app.use(cors(corsOptions));
 app.use(morgan("dev"));
 
 app.get("/pong", pongController);
